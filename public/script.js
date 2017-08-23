911e551ddf780f6c080eb1c88ceac20300f83890
Max Devel <fbeerli@nomos-system.com>
2017-08-23 15:58:39 +0200

    version 0.0.05 with error

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 $('.js-xhr').on('click', function(e) {
 	e.preventDefault();
-	var link = $(this).prop('href');
-	alert("class js-xhr click: " + link);
+	var link = $(this).data('path');
+	//alert("class js-xhr click: " + link);
 	$.ajax({ type: 'GET', url: link,
 		success: function(resp) {
 			console.log(resp);
--- server.js
+++ server.js
@@ -2,19 +2,26 @@ const express = require('express')
 const app = express()
 const exec = require('child_process').exec;
 
+app.set('view engine', 'ejs');
 
 app.use('/public', express.static('public'));
 app.use('/img', express.static('img'));
 
+// app.get('/', function (req, res) {
+// 	//createHtml();
+// 	console.log( getDateTime() + " -> (GET) " + req.url );
+// 	res.sendFile(__dirname + '/public/index.html');
+// })
 app.get('/', function (req, res) {
-	console.log( getDateTime() + " -> (GET) " + req.url );
-	res.sendFile(__dirname + '/public/index.html');
+    console.log( getDateTime() + " -> (GET) " + req.url );
+    res.render('index');
 })
 
 app.get('/:id', function(req, res) {
-	console.log('User-Agent: ' + req.headers['user-agent']);
+	//console.log('User-Agent: ' + req.headers['user-agent']);
 	console.log( getDateTime() + " -> (GET) " + req.url );
 	exec( "osascript ./apple_script/" + req.params.id );
+    res.end();      // otherwise Safari makes request all 2 Minutes
 });
 
 
@@ -39,4 +46,16 @@ function getDateTime() {
     var day  = date.getDate();
     day = (day < 10 ? "0" : "") + day;
     return year + "-" + month + "-" + day + "  " + hour + ":" + min + ":" + sec;
+}
+
+function createHtml(){
+    console.log("createHtml function");
+    const testFolder = './script/';
+    const fs = require('fs');
+
+    fs.readdir(testFolder, (err, files) => {
+        files.forEach(file => {
+            console.log(file);
+        });
+    })
 }
