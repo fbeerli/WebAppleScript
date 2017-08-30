f3cc02d0ac321d7396ea48156725e817425d0783
Max Devel <fbeerli@nomos-system.com>
2017-08-30 15:34:56 +0200

    0.1.0 code refactoring, reload button

--- public/script.js
+++ public/script.js
@@ -5,13 +5,20 @@
 	white: true
 */
 
-$('.js-xhr').on('click', function(e) {
-	e.preventDefault();
-	var link = $(this).data('path');
-	//alert("class js-xhr click: " + link);
-	$.ajax({ type: 'GET', url: link,
+$( document ).ready( function (){
+	eventClickReload();
+});
+
+$('#div_reload').on('click', function(e){
+	//alert( "click on reload - function not yet implemented" );
+	$.ajax({ 
+		type: 'POST', 
+		url: "http://localhost:3000", 
+		data: { action: 'getList' },
 		success: function(resp) {
 			console.log(resp);
+			//alert( "AJAX success: \n" + resp.ip );
+			createNewFileList( resp.fileList, resp.ip );
 		},
 		error: function(err) {
 			console.log(err);
@@ -19,20 +26,34 @@ $('.js-xhr').on('click', function(e) {
 	});
 });
 
-$('.btn_execute').on('click', function(e) {
-	e.preventDefault();
-	var link = $(this).data('path');
-	//alert("class btn_execute click: " + link);
-	$.ajax({ type: 'GET', url: link,
-		success: function(resp) {
-			console.log(resp);
-		},
-		error: function(err) {
-			console.log(err);
-		}
-	});
-});
-
+function createNewFileList( vFileList, vIp ){
+	$('.div_list').empty().hide();
+	var vContent = '' + 
+		'<table class="tbl_list">' +
+            '<tr><th>AppleScript<div id="div_reload" title="Reload Directory"><img src="/img/reload.png"></div></th>' +
+                '<th>Copy Link</th><th>Start</th>' +
+            '</tr>';
+    for( var i = 0; i < vFileList.length; i++ ){
+    	vContent += '<tr>' +
+    				'<td class="js-xhr td_as" data-path="http://' + vIp + ':3000/' + vFileList[i] + '">' +
+                        '<a href="" data-path="' + vFileList[i] + '" class="js-xhr">' + vFileList[i] + '</a>' +
+                    '</td>' +
+                    '<td>' +
+                        '<button type="button" class="btn_copy" data-path="http:// ' + vIp + ':3000/' + vFileList[i] + '">' +
+                            'Copy URL' +
+                        '</button>' +
+                    '</td>' +
+                    '<td>' +
+                        '<button type="button" class="js-xhr" data-path="'+ vFileList[i] + '">' +
+                            'Execute' +
+                        '</button>' +
+                    '</td>' +
+                   	'</tr>';
+    }
+    vContent += '</table>'; 
+	$(".div_list").append( vContent ).show();
+	eventClickReload();
+};
 
 $('.btn_copy').on('click', function(e) {
 	e.preventDefault();
@@ -50,8 +71,23 @@ $('.btn_copy').on('click', function(e) {
   	document.body.removeChild(ipt_url);
 });
 
+function eventClickReload(){
+	$('.js-xhr').unbind();
+	$('.js-xhr').on('click', function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		var link = $(this).data('path');
+		alert("class js-xhr click: " + link);
+		$.ajax({ type: 'GET', url: link,
+			success: function(resp) {
+				alert( resp );
+				console.log(resp);
+			},
+			error: function(err) {
+				console.log(err);
+			}
+		});
+	});
+}
 
-$('.td_url').on('click', function(e) {
-	e.preventDefault();
-	//$(this).select();
-});
+
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express   = require('express')
-const app       = express()
-const exec      = require('child_process').exec;
-const path      = require('path');
-const fs        = require('fs');
-const os        = require( 'os');
+const express       = require('express');
+const app           = express();
+const exec          = require('child_process').exec;
+const path          = require('path');
+const fs            = require('fs');
+const os            = require('os');
+const bodyParser    = require('body-parser');
+const util          = require('util');
+
+
+const folder = './apple_script/';
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -12,22 +17,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.use('/public', express.static('public'));
 app.use('/img', express.static('img'));
 
+app.use( bodyParser.json() );                            // to support JSON-encoded bodies
+app.use( bodyParser.urlencoded({ extended: true }));     // to support URL-encoded bodies 
+
 
 app.get('/', function (req, res) {
     console.log( getDateTime() + " -> (GET) " + req.url );
-    getFileList( res );
+    renderFileListForIndex( res );
 })
 
 app.get('/:id', function(req, res) {
 	//console.log('User-Agent: ' + req.headers['user-agent']);
-    let as = req.params.id;
-	console.log( getDateTime() + " -> (GET:Execute) " + as );
-    //console.log(as);
-
-	exec( "osascript './apple_script/" + as + "'" );           // put the file name in simple quotation marks
-    res.end();      // otherwise Safari makes request all 2 Minutes
+    executeScript( req, res );
 });
 
+app.post('/', function(req, res){
+    //console.log( getDateTime() + " -> (POST:action) \"" + req.params.action + "\" " );
+    //res.send( "State OK:\nAJAX request: " + req.params.action + "\n");
+    console.log( getDateTime() + " -> POST: action = \"" + req.body.action + "\"" );
+    //res.end( "action: " + req.body.action );
+    createFileListAndSendToClient( res );
+});
 
 app.listen(3000, function () {
 	console.log('Listening on http://localhost:3000')
@@ -52,9 +62,8 @@ function getDateTime() {
     return year + "-" + month + "-" + day + "  " + hour + ":" + min + ":" + sec;
 }
 
-function getFileList( res ){
-    //console.log("getFileList() function");
-    const folder = './apple_script/';
+function renderFileListForIndex( res ){
+    //console.log("renderFileListForIndex() function");
     var vIp = getServerIpAddress();
 
     fs.readdir(folder, (err, files) => {
@@ -63,17 +72,32 @@ function getFileList( res ){
             console.log( " - - > Error while reading directory: " + err );
         }else if( files ){
          files.forEach(file => {
-            //let regex = /^[a-zA-Z0-9 _äÄöÖüÜ-][a-zA-Z0-9 _äÄöÖüÜ-]+\.(scpt|scptd|app|applescript)$/;
-            //let regex = /^[.]+\.(scpt|scptd|app|applescript)$/;
-            //let regex = /^[a-zA-Z0-9 _\-\u00e4]+\.(scpt|scptd|app|applescript)$/;
-            let regex = /^[\w \-]+\.(scpt|scptd|app|applescript)$/;
-            if( file.match( regex ) ) myList.push(file);
+            if( file.match(  getFileRegex()  ) ) myList.push(file);
         });
-            //console.log( "getFileList(): files.length: " + files.length );
+            //console.log( "renderFileListForIndex(): files.length: " + files.length );
             //console.log( vIp );
             res.render( 'index', { posts: myList, ip: vIp } );
         }else{
-            console.log(" - - > getFileList, ELSE that never should happen");
+            console.log(" - - > renderFileListForIndex, ELSE that never should happen");
+        }
+    });
+}
+
+function createFileListAndSendToClient( res ){
+    var vIp = getServerIpAddress();
+    fs.readdir(folder, (err, files) => {
+        let myList = [];
+        if( err ) {
+            console.log( " - - > Error while reading directory: " + err );
+        }else if( files ){
+            files.forEach(file => {
+            if( file.match(  getFileRegex()  ) ) myList.push(file);
+        });
+            //console.log( "renderFileListForIndex(): files.length: " + files.length );
+            //console.log( vIp );
+            res.send( { fileList: myList, ip: vIp } );
+        }else{
+            console.log(" - - > renderFileListForIndex, ELSE that never should happen");
         }
     });
 }
@@ -81,16 +105,13 @@ function getFileList( res ){
 function getServerIpAddress(){
     var vIp = 0;
     var ifaces = os.networkInterfaces();
-
     Object.keys(ifaces).forEach(function (ifname) {
         var alias = 0;
-
         ifaces[ifname].forEach(function (iface) {
             if ('IPv4' !== iface.family || iface.internal !== false) {
                 // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
                 return;
             }
-
             if (alias >= 1) {
                 // this single interface has multiple ipv4 addresses
                 // console.log(ifname + ':' + alias, iface.address);
@@ -105,6 +126,51 @@ function getServerIpAddress(){
         });
     });
     return vIp;
+}
+
+function executeScript( req, res ){
+    let vScript = req.params.id;
+    if( vScript.match(  getFileAppleScriptRegex()  ) && fs.existsSync( folder + vScript ) ){
+        console.log( getDateTime() + " -> (GET:Execute AppleScript) \"" + vScript + "\"");
+        exec( "osascript './apple_script/" + vScript + "'", function(error, stdout, stderr) {
+            res.send( "State OK:\n + Script \"" + vScript + "\" has been execueted\n" );
+            res.send( "stderr: " + stderr );
+            res.end( stdout );
+        });
+    }else if( vScript.match(  getFileBashRegex()  ) && fs.existsSync( folder + vScript ) ){
+        console.log( getDateTime() + " -> (GET:Execute bash script) \"" + vScript + "\"");
+        //var vBashResult = exec( "bash './apple_script/" + vScript + "'" ); 
+        
+        exec( "bash './apple_script/" + vScript + "'", function(error, stdout, stderr){ 
+            res.send( "stderr:\n" + stderr );
+            res.end( stdout );
+            // res.end() - otherwise Safari makes request all 2 Minutes
+        });
+
+    }else if( vScript.match( getFileRegex() ) && !fs.existsSync( folder + vScript ) ){
+        console.log( getDateTime() + " -> (GET:Execute) \"" + vScript + "\" file not available" );
+        res.send( "State Error:\n - Script \"" + vScript + "\" not available\n" );
+        res.end();      // otherwise Safari makes request all 2 Minutes
+    }else{
+        console.log( getDateTime() + " -> (GET:Execute) \"" + vScript + "\" " );
+        res.send( "State Error:\n - Script \"" + vScript + "\" invalid character in filename\n" );
+        res.end();      // otherwise Safari makes request all 2 Minutes
+    }
+}
+
+function getFileRegex(){
+    // defines the carachters allowed in files to list and execute it
+    return /^[\w \-]+\.(scpt|scptd|app|applescript|bs)$/;
+}
+
+function getFileAppleScriptRegex(){
+    // defines the carachters allowed in files to list and execute it
+    return /^[\w \-]+\.(scpt|scptd|app|applescript)$/;
+}
+
+function getFileBashRegex(){
+    // defines the carachters allowed in files to list and execute it
+    return /^[\w \-]+\.(bash|bs)$/;
 }
 
 
