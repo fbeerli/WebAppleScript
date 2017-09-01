d148d575cd42ee031c578360e39c284c3aa15b54
Max Devel <fbeerli@nomos-system.com>
2017-09-01 11:27:18 +0200

    delete files implemented

--- public/script.js
+++ public/script.js
@@ -4,6 +4,9 @@
 	regexp: true, sloppy: true, vars: false,
 	white: true
 */
+
+
+var vAdminMode = false;
 var vServerIP;
 var vServerPort;
 
@@ -22,13 +25,30 @@ $( document ).ready( function (){
 		}
 	});
 	enableEventListener();
+	keyboardEventHandler();
 });
+
+function keyboardEventHandler(){
+    $(document).unbind().keydown();
+    $(document).unbind().keyup();
+   	$(document).keyup(function(event) {
+        event = event || window.event;
+        event.stopPropagation();
+        if( event.keyCode == 27 ) switchAdminMode( false );                                 // 27 == ESC
+        if( event.keyCode == 69 ) switchAdminMode(); 										// 69 == e == edit == admin mode
+        if( event.keyCode == 82 ) $('#div_reload').click();									// 82 == r == reload
+    });
+}
 
 function createNewFileList( vFileList, ip ){
 	$('.div_list').empty().hide();
 	var vContent = '' + 
 		'<table class="tbl_list">' +
-            '<tr id="tr_header"><th>AppleScript<div id="div_reload" title="Reload Directory"><img src="/img/reload.png"></div></th>' +
+            '<tr id="tr_header">' +
+	            '<th>AppleScript' +
+	            	'<div id="div_reload" title="Reload Directory"><img src="/img/reload.png"></div>' +
+	            	'<img id="add_file" class="cls_admin" src="/img/add.png">' +
+	            '</th>' +
                 '<th>Copy Link</th><th>Start</th>' +
             '</tr>';
     for( var i = 0; i < vFileList.length; i++ ){
@@ -47,6 +67,12 @@ function createNewFileList( vFileList, ip ){
                         'Execute' +
                     '</button>' +
                 '</td>' +
+                '<td class="cls_admin cls_edit">' +
+                    '<img data-path="'+ vFileList[i] + '" src="/img/edit.png">' +
+                '</td>' +
+                '<td class="cls_admin cls_delete">' +
+                    '<img class="" data-path="'+ vFileList[i] + '" src="/img/delete.png">' +
+                '</td>' +
            	'</tr>';
     }
     vContent += '</table>'; 
@@ -54,6 +80,7 @@ function createNewFileList( vFileList, ip ){
 	enableEventListener();
 };
 function createReloadFinished(){
+	// change the relaod button so the user knows that something is going on
 	var vContent = '' + 
         '<th>AppleScript<div id="div_reload" style="color:green;">OK</div></th>' +
         '<th>Copy Link</th><th>Start</th>';
@@ -61,9 +88,16 @@ function createReloadFinished(){
 }
 function createReloadButton(){
 	var vContent = '' + 
-        '<th>AppleScript<div id="div_reload" title="Reload Directory"><img src="/img/reload.png"></div></th>' +
+        '<th>AppleScript' +
+        	'<div id="div_reload" title="Reload Directory"><img src="/img/reload.png"></div>' +
+        	'<img id="add_file" class="cls_admin" src="/img/add.png">' +
+        '</th>' +
         '<th>Copy Link</th><th>Start</th>';
 	$('#tr_header').empty().append( vContent ).show();
+	if(vAdminMode){
+		vAdminMode = false;
+		switchAdminMode();
+	}
 }
 
 function enableEventListener(){
@@ -117,6 +151,34 @@ function enableEventListener(){
 				}
 		});
 	});
+	$('#add_file').unbind();
+	$('#add_file').on('click', function(e){
+		alert( "Add new File to list - is not yet implemented") ;
+	});
+	$('.cls_edit').unbind();
+	$('.cls_edit').on('click', function(e){
+		alert( "Edit File is not yet implemented") ;
+	});
+	$('.cls_delete').unbind();
+	$('.cls_delete').on('click', function(e){
+		var link = $(this).children().data('path');
+		if( confirm( 'Delete file: "' + link + '" on WEB-SERVER?') ){
+			$.ajax({ 
+				type: 'POST', 
+				data: { action: 'deleteFile', filename: link },
+				success: function(resp) {
+					console.log(resp);
+					createNewFileList( resp.fileList, resp.ip );
+					createReloadFinished();
+					setTimeout(function(){ createReloadButton(), enableEventListener() }, 1000);
+				},
+				error: function (jqXHR, exception) {
+					errorHandlingAjax( jqXHR, exception );
+					console.log(jqXHR);
+					}
+			});
+		}
+	});
 }
 
 function errorHandlingAjax( jqXHR, exeption ){
@@ -140,3 +202,18 @@ function errorHandlingAjax( jqXHR, exeption ){
 	console.log(jqXHR);
 }
 
+function switchAdminMode( newAdminMode ){                 // ADMIN MODE is to switch website in an EDIT MODE
+    //  switch edit mode on/off
+    if( newAdminMode === false ){
+    	vAdminMode = true;
+    }else if( newAdminMode === true ){
+    	vAdminMode = false;
+    }
+    if( vAdminMode ){ 
+        vAdminMode = false;
+        $('.cls_admin').hide();
+    }else{ 
+        vAdminMode = true;
+        $('.cls_admin').show(); 
+    };
+}
--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post('/', function(req, res){
     //res.send( "State OK:\nAJAX request: " + req.params.action + "\n");
     console.log( getDateTime() + " -> POST: action = \"" + req.body.action + "\"" );
     //res.end( "action: " + req.body.action );
-    createFileListAndSendToClient( res );
+    validateAjaxAction( req, res );
 });
 
 app.listen(3000, function () {
@@ -58,20 +58,39 @@ app.listen(vServerPort, vServerIp, function () {
 
 
 
-function getDateTime() {
-    var date = new Date();
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-    var min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-    var sec  = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
-    var day  = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
-    return year + "-" + month + "-" + day + "  " + hour + ":" + min + ":" + sec;
+
+
+
+function validateAjaxAction( req, res ){
+    if( req.body.action === 'deleteFile'){
+        deleteFile( req, res )
+    }else if( req.body.action === 'getList'){
+        createFileListAndSendToClient( res );
+    }else{
+        console.log( getDateTime() + ' -> ERROR - this action: "' + req.body.action 
+            + '" is not defined in function "validateAjaxAction"' );
+    }
+}
+
+function deleteFile( req, res ){
+    if( fs.lstatSync( vScriptFolder + req.body.filename ).isFile() ){
+        fs.unlink( vScriptFolder + req.body.filename, function( err ){
+            if( err ){
+                console.log( getDateTime() + ' -> ERROR while try to delete file: "' + vScriptFolder + req.body.filename + '"' );
+            }else{
+                console.log( getDateTime() + ' -> DELETE file: "' + vScriptFolder + req.body.filename + '"' );
+            }
+            createFileListAndSendToClient( res );
+        })
+    }else if( fs.lstatSync( vScriptFolder + req.body.filename ).isDirectory() ){
+        //console.log( getDateTime() + ' -> is a folder - NOT deleted: "' + vScriptFolder + req.body.filename + '"' );
+        console.log( getDateTime() + ' -> DELETE folder: "' + vScriptFolder + req.body.filename + '"' );
+        rmdir( vScriptFolder + req.body.filename );
+        createFileListAndSendToClient( res );
+    }else{
+        console.log( getDateTime() + ' -> NO file nor folder (NO action): "' + vScriptFolder + req.body.filename + '"' );
+        res.end();
+    }
 }
 
 function renderFileListForIndex( res ){
@@ -114,32 +133,6 @@ function createFileListAndSendToClient( res ){
     });
 }
 
-function getServerIpAddress(){
-    var vIp = 0;
-    var ifaces = os.networkInterfaces();
-    Object.keys(ifaces).forEach(function (ifname) {
-        var alias = 0;
-        ifaces[ifname].forEach(function (iface) {
-            if ('IPv4' !== iface.family || iface.internal !== false) {
-                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-                return;
-            }
-            if (alias >= 1) {
-                // this single interface has multiple ipv4 addresses
-                // console.log(ifname + ':' + alias, iface.address);
-                //return iface.address;
-            } else {
-                // this interface has only one ipv4 adress
-                // console.log("AAAA: ", iface.address);
-                vIp = iface.address;
-                // console.log("IP: ", vIp);
-            }
-            ++alias;
-        });
-    });
-    return vIp;
-}
-
 function verifyScript( req, res ){
     let vScript = req.params.id;
     if(! vScript.match( getFileRegex( "file" ) ) ){
@@ -173,6 +166,10 @@ function executeScript( vExe, vScript, res ){
     });
 }
 
+
+
+//  INTERNAL FUNCTION
+
 function getFileRegex( vType ){
     // defines the carachters allowed in files to list and execute it
     if( vType == "file" ){
@@ -183,6 +180,67 @@ function getFileRegex( vType ){
         return /^[\w \.\-]+\.(sh|bs|bash)$/i;
     }
 }
+
+function getServerIpAddress(){
+    var vIp = 0;
+    var ifaces = os.networkInterfaces();
+    Object.keys(ifaces).forEach(function (ifname) {
+        var alias = 0;
+        ifaces[ifname].forEach(function (iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+            }
+            if (alias >= 1) {
+                // this single interface has multiple ipv4 addresses
+                // console.log(ifname + ':' + alias, iface.address);
+                //return iface.address;
+            } else {
+                // this interface has only one ipv4 adress
+                // console.log("AAAA: ", iface.address);
+                vIp = iface.address;
+                // console.log("IP: ", vIp);
+            }
+            ++alias;
+        });
+    });
+    return vIp;
+}
+
+function getDateTime() {
+    var date = new Date();
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+    return year + "-" + month + "-" + day + "  " + hour + ":" + min + ":" + sec;
+}
+
+var rmdir = function( dir ) {
+    var list = fs.readdirSync(dir);
+    for(var i = 0; i < list.length; i++) {
+        var filename = path.join(dir, list[i]);
+        var stat = fs.statSync(filename);
+        
+        if(filename == "." || filename == "..") {
+            // pass these files
+        } else if(stat.isDirectory()) {
+            // rmdir recursively
+            rmdir(filename);
+        } else {
+            // rm fiilename
+            fs.unlinkSync(filename);
+        }
+    }
+    fs.rmdirSync(dir);
+};
 
 
 
